3454c169868309d55e5771cbf0794df58ed4b79b
bhargavrohit33 <68465450+bhargavrohit33@users.noreply.github.com>
2023-06-23 12:38:34 -0400

    Second Test Created

--- MidTesting/TestClass.cs
+++ MidTesting/TestClass.cs
@@ -6,14 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MidTermTesting
+namespace MidTesting
 {
     [TestFixture]
     public class TestClass
     {
         [Test]
 
-
         public void CalculatePPI_Input2000and1500and13dot4_Output220dot53and0dot1152and3396()
         {
             //Arrange 
@@ -31,17 +30,17 @@ namespace MidTermTesting
             //Assert 
             Assert.AreEqual(expected, actual);
         }
-         [Test]
 
+        [Test]
 
-        public void CalculatePPI_Input2000and1500and13dot4_Output220dot53and0dot1152and3396()
+        public void CalculatePPI_Input10and20and30_Output0dot75and35dot0777and22()
         {
             //Arrange 
-            double screenWidth = 2000;
-            double screenHeight = 1500;
-            double diagonalSize = 13.4;
+            double screenWidth = 10;
+            double screenHeight = 20;
+            double diagonalSize = 30;
 
-            string expected = "\n\nPPI: " + 186.57 + " \nDot Pitch: " + 0.1361 + " \nDiagonal Size in Pixels: " + 2500;
+            string expected = "\n\nPPI: " + 0.75 + " \nDot Pitch: " + 34.0777 + " \nDiagonal Size in Pixels: " + 22;
 
             //Act
             Dictionary<string, double> myDictionary = new Dictionary<string, double>();
@@ -51,5 +50,6 @@ namespace MidTermTesting
             //Assert 
             Assert.AreEqual(expected, actual);
         }
+
     }
 }
